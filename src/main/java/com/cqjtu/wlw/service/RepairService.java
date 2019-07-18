df9dad168e9ba06f3284f9c69d71e01c3b95283b
cungudafa <34543897+cungudafa@users.noreply.github.com>
2019-07-18 08:59:59 +0800

    Add files via upload

---
+++ src/main/java/com/cqjtu/wlw/service/RepairService.java
@@ -0,0 +1,38 @@
+package com.cqjtu.wlw.service;
+
+import com.cqjtu.wlw.pojo.RepairInfo;
+
+import java.util.List;
+
+public interface RepairService {
+    /**
+     * Repair注册
+     * @param repairInfo
+     */
+    void regRepairInfo(RepairInfo repairInfo);
+
+    /**
+     * 维修记录注销
+     * @param repairInfo
+     */
+    void delRepairInfo(RepairInfo repairInfo);
+
+    /**
+     * 维修记录信息更新
+     * @param repairInfo
+     */
+    void updateRepairInfo(RepairInfo repairInfo);
+
+    /**
+     * 根据条件查询多条维修记录信息
+     * @param repairInfo
+     * @return
+     */
+    List<RepairInfo> getRepairInfos(RepairInfo repairInfo);
+    /**
+     * 根据id查询记录
+     * @param repairInfo
+     * @return
+     */
+    RepairInfo getRepairById(RepairInfo repairInfo);
+}
