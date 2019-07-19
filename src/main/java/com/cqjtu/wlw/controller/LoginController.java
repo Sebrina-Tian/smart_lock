1286fc9f685ed3834bffd6695ad17b2b0d6f553c
Sebrina-Tian <48372371+Sebrina-Tian@users.noreply.github.com>
2019-07-19 18:01:48 +0800

    Add files via upload

--- src/main/java/com/cqjtu/wlw/controller/LoginController.java
+++ src/main/java/com/cqjtu/wlw/controller/LoginController.java
@@ -1,92 +1,76 @@
 package com.cqjtu.wlw.controller;
 
+import com.cqjtu.wlw.dao.ClientDao;
 import com.cqjtu.wlw.pojo.ClientInfo;
+import com.cqjtu.wlw.pojo.ManagerInfo;
 import com.cqjtu.wlw.pojo.UserInfo;
 import com.cqjtu.wlw.pojo.WorkerInfo;
 import com.cqjtu.wlw.service.ClientService;
+import com.cqjtu.wlw.service.ManagerService;
 import com.cqjtu.wlw.service.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
 public class LoginController {
-    @Autowired//自动装配，将ClientService接口实现的类自动注入进来
+
+    @Autowired
     private ClientService clientService;
     @Autowired
     private WorkerService workerService;
-    //@Autowired
-    //private ManagerService managerService;
+    @Autowired
+    private ManagerService managerService;
 
     @RequestMapping(value = "/login")
     public String login(UserInfo a, HttpSession session) {
         System.out.println("用户:" + a.getUserId() +"\t密码：" + a.getUserPassword()+"\t身份："+ a.getUserIdentity());
         session.setAttribute("user", a);
-
+        String user_id=a.getUserId();
+        String user_password= a.getUserPassword();
         String user_page = a.getUserIdentity();//client、manager、worker
 
-        return user_page;
-    }
-    @RequestMapping("/check")
-    public String doPost(UserInfo userInfo, HttpServletResponse resp, HttpServletRequest request) {
-
-            String user_id = userInfo.getUserId();
-            String user_password = userInfo.getUserPassword();
-            System.out.println("客户登录：" + user_id + "\t密码：" + user_password + "\t身份：" + userInfo.getUserIdentity());
-            String identity = userInfo.getUserIdentity();
-            if (identity == "client") {
-                ClientInfo clientInfo = new ClientInfo();
-                clientInfo.setClientId(user_id);
-                String password = clientService.getClientById(clientInfo).getClientPassword();
-                System.out.println(password);
-                if (user_password == password) {
-                    try {
-                        System.out.println("客户登录成功");
-                        return "client";
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.out.println("客户登录失败");
-                    }
-                    return "homepage";
-                }
-            } else if (identity == "worker") {
-                WorkerInfo workerInfo = new WorkerInfo();
-                workerInfo.setWorkerId(user_id);
-                String password = workerService.getWorkerById(workerInfo).getWorkerPassword();
-                System.out.println(password);
-                if (user_password == password) {
-                    try {
-                        System.out.println("维修人员登录成功");
-                        return "worker";
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.out.println("维修工失败");
-                    }
-                    return "homepage";
-                }
-            }else if (identity == "manager") {
-                return "manager";
-//                ManagerInfo managerInfo = new ManagerInfo();
-//                managerInfo.setManagerId(user_id);
-//                String password="1";
-//                //String password = managerService.getWorkerById(workerInfo).getWorkerPassword();
-//                System.out.println(password);
-//                if (user_password == password) {
-//                    try {
-//                        return "manager";
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                        System.out.println("管理员登录失败");
-//                    }
-//                    return "homepage";
-//                }
+        //当用户身份为client时，下同
+        if(user_page.equals("client")){
+            ClientInfo clientInfo=new ClientInfo();   //新建一个ClientInfo类
+            clientInfo.setClientId(user_id);
+            ClientInfo client = clientService.getClientById(clientInfo);   //调用ClientService方法，查询id等于user_id的用户信息，并返回ClientInfo类对象：client
+            if(client != null && client.getClientPassword().equals(user_password)){    //判断client_password是否与user_password相等，若相等则跳转至用户页面
+                    return user_page;
             }
+        }
+
+        if(user_page.equals("worker")){
+            WorkerInfo workerInfo=new WorkerInfo();
+            workerInfo.setWorkerId(user_id);
+            WorkerInfo worker=workerService.getWorkerByWorkerId(workerInfo);
+            if(worker !=null && worker.getWorkerPassword().equals(user_password)){
+                return user_page;
+            }
+        }
+        if(user_page.equals("manager")){
+            int userid=Integer.parseInt(user_id);  //获取到前端输入的user_id为String类型，但MAanager_id是设置为int类型的，此处需转换
+            ManagerInfo managerInfo=new ManagerInfo();
+            managerInfo.setManagerId(userid);
+            ManagerInfo manager=managerService.getManagerById(managerInfo);
+            if(manager!=null && manager.getManagerPassword().equals(user_password)){
+                return user_page;
+            }
+        }
             return "homepage";
+    }
+    @RequestMapping(value = "/check")
+    public String login_check(UserInfo a, HttpServletRequest request, HttpSession session) {
+
+        System.out.println("用户:" + a.getUserId() +"\t密码：" + a.getUserPassword()+"\t身份："+ a.getUserIdentity());
+        session.setAttribute("user", a);
+
+        String user_page = a.getUserIdentity();//client、manager、worker
+        return user_page;
     }
 
 }
